5149585a9140309eea18f1b45a810a021c340f42
EduardoMascarenhas <eduardomsmascarenhas@gmail.com>
2021-10-26 17:49:09 -0300

    ajustes

--- models/eixo.js
+++ models/eixo.js
@@ -1,51 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
 const eixoSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            trim: true,
-            min: 3,
-            max: 160,
-            required: true
-        },
-        subTitle: {
-            type: String,
-            trim: true,
-            min: 3,
-            max: 160,
-        },
-        slug: {
-            type: String,
-            unique: true,
-            index: true
-        },
-        body: {
-            type: {},
-            required: true,
-        },
-        excerpt: {
-            type: String,
-            max: 1000
-        },
-        mtitle: {
-            type: String
-        },
-        mdesc: {
-            type: String
-        },
-        thumb: {
-            data: Buffer,
-            contentType: String
-        },
-        projetos: [{ type: ObjectId, ref: 'Projeto', required: true }],
-        postedBy: {
-            type: ObjectId,
-            ref: 'User'
-        }
+  {
+    title: {
+      type: String,
+      trim: true,
+      min: 3,
+      max: 160,
+      required: true,
     },
-    { timestamps: true }
+    subTitle: {
+      type: String,
+      trim: true,
+      min: 3,
+      max: 160,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      index: true,
+    },
+    vis: {
+      type: {},
+      required: true,
+    },
+    met: {
+      type: {},
+      required: true,
+    },
+    pro: {
+      type: {},
+      required: true,
+    },
+    borderColor: {
+      type: String,
+      required: true,
+    },
+    thumb: {
+      data: Buffer,
+      contentType: String,
+    },
+    projetos: [{ type: ObjectId, ref: "Projeto", required: true }],
+    postedBy: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('Eixo', eixoSchema);
+module.exports = mongoose.model("Eixo", eixoSchema);
