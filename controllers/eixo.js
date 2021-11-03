030859198ea1c8e159595d980c257572be398835
EduardoMascarenhas <eduardomsmascarenhas@gmail.com>
2021-11-03 15:20:13 -0300

    add

--- controllers/eixo.js
+++ controllers/eixo.js
@@ -229,7 +229,7 @@ exports.update = (req, res) => {
       if (projetos) {
         oldEixo.projetos = projetos.split(",");
       }
-
+      console.log(files.thumb);
       if (files.thumb) {
         if (files.thumb.size > 10000000) {
           return res.status(400).json({
